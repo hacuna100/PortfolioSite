b67c76e02d210aefa5a799b48b37f2ff05485e6b
Hermilo Acuna <HAcuna100@gmail.com>
2018-01-18 12:22:53 -0500

    Created content for site as well as images for the portfolio.

---
+++ PortfolioSite/PortfolioSite.WebUI/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Optimization;
+
+namespace PortfolioSite.WebUI
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.Bundles.UseCdn = true;
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.min.css",
+                "~/Content/font-awesome.min.css",
+                "~/Content/Site.css"));
+            bundles.Add(new ScriptBundle("~/Scripts/scripts").Include(
+                "~/Scripts/jquery-3.2.1.min.js",
+                "~/Scripts/jquery-site.js",
+                "~/Scripts/modernizr-2.8.3.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/Site.js"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ PortfolioSite/PortfolioSite.WebUI/Scripts/Site.js
@@ -0,0 +1,20 @@
+﻿function initMap() {
+    var myLatLng = { lat: 38.2559691, lng: -85.7641486 };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: "Nosey Aren't ya"
+    });
+}
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments) };
+gtag('js', new Date());
+
+gtag('config', 'UA-107099444-1');
