0d3349c8250e0e5cbbdab99f4a372e1036d9b4bb
Hermilo Acuna <HAcuna100@gmail.com>
2018-01-18 11:39:17 -0500

    Added controllers and views

---
+++ PortfolioSite/PortfolioSite.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioSite.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Resume()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult ErrorPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioSite/PortfolioSite.WebUI/Controllers/SharedController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioSite.WebUI.Controllers
+{
+    public class SharedController : Controller
+    {
+        public PartialViewResult NavBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult Footer()
+        {
+            return PartialView();
+        }
+    }
+}
